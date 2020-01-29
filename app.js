45ea68c0afe4bed50a04da1bf3ab129746ae5e70
Patrick Sime <psime86@gmail.com>
2020-01-29 01:05:11 -0600

    Firebase set up

---
+++ app.js
@@ -0,0 +1,40 @@
+ // Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBzoAwq-SMpZKplkXvAF9lRhcs9V8iqDQ4",
+    authDomain: "simpletrainman.firebaseapp.com",
+    databaseURL: "https://simpletrainman.firebaseio.com",
+    projectId: "simpletrainman",
+    storageBucket: "simpletrainman.appspot.com",
+    messagingSenderId: "496240507858",
+    appId: "1:496240507858:web:c6e6f34a7161774196a60b",
+    measurementId: "G-WNEH6ENNC0"
+  };
+
+// Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+
+// Create a variable to reference the database
+    var database = firebase.database();
+
+// Current time
+    var currentTime = moment().format();
+    console.log(currentTime);
+
+// User input
+
+
+
+// Create user train from input and push user train to table
+
+
+
+// Clear user input form
+
+
+
+// Firebase snapshot variables
+
+
+
+// Time variables
