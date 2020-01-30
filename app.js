06f91d07a00708fde028ff0d8b9afc25963d0391
Patrick Sime <psime86@gmail.com>
2020-01-30 01:21:26 -0600

    IT WORKS! and I am so very tired

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+ $(document).ready(function(){
+ 
  // Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyBzoAwq-SMpZKplkXvAF9lRhcs9V8iqDQ4",
@@ -16,25 +18,75 @@ var firebaseConfig = {
 
 // Create a variable to reference the database
     var database = firebase.database();
+    console.log(database);
 
 // Current time
     var currentTime = moment().format();
     console.log(currentTime);
 
 // User input
+$("#submit").on("click", function(){
+    event.preventDefault();
+
+    var trainNameInput = $("#TrainNameInput").val().trim();
+    var destinationInput = $("#DestinationInput").val().trim();
+    var firstTrainTimeInput = $("#FirstTrainTimeInput").val().trim();
+    var frequencyInput = $("#FrequencyInput").val().trim();
 
 
 
-// Create user train from input and push user train to table
+// Create user train from input and push user train to database
+    var userTrain = {
+        train: trainNameInput,
+        destination: destinationInput,
+        first: firstTrainTimeInput,
+        frequency: frequencyInput,
+    };
 
-
+    database.ref().push(userTrain);
 
 // Clear user input form
-
-
+    $("#TrainNameInput").val("");
+    $("#DestinationInput").val("");
+    $("#FirstTrainTimeInput").val("");
+    $("#FrequencyInput").val("");
+})
 
 // Firebase snapshot variables
+database.ref().on("child_added", function(childSnapshot){
 
+    var trainNameInput = childSnapshot.val().train;
+    var destinationInput = childSnapshot.val().destination;
+    var firstTrainTimeInput = childSnapshot.val().time;
+    var frequencyInput = childSnapshot.val().frequency;
 
+// Time variables //
+    
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTrainTimeInput, "HH:mm").subtract(1, "years");
 
-// Time variables
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+    // Time apart (remainder)
+    var remainder = diffTime % frequencyInput;
+
+    // Minute Until Train
+    var minutesUntilTrain = frequencyInput - remainder;
+
+    // Next Train
+    var nextArrival = moment().add(minutesUntilTrain, "minutes").format("HH:mm");
+
+    // Append to new row in HTML
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainNameInput),
+        $("<td>").text(destinationInput),
+        $("<td>").text(frequencyInput),
+        $("<td>").text(nextArrival),
+        $("<td>").text(minutesUntilTrain),
+    );
+
+    $("#TrainTable > tbody").append(newRow);
+})
+
+})
